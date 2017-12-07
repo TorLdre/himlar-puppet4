61d1bc7b70708d0083c4a2b6c7ca982b58c2d2da
TorLdre <tor.ledre@gmail.com>
2017-12-07 11:03:01 +0100

    basic ipsec install and fw config

---
+++ profile/manifests/network/ipsec.pp
@@ -0,0 +1,39 @@
+#
+class profile::network::ipsec(
+  $enable          = false,
+  $config_dir      = '/etc/ipsec.d/',
+  $manage_firewall = false,
+) {
+
+  if enable_ipsec {
+    package { 'libreswan':
+      ensure   => installed
+    }
+    service { 'ipsec':
+      ensure     => running,
+      enable     => true,
+      require    => Package['libreswan']
+    }
+    if $manage_firewall {
+      profile::firewall::rule { '914 ipsec allow IKE':
+        proto    => 'udp',
+        port     => ['500','4500'],
+      }
+      profile::firewall::rule { '914 ipsec allow IKE IPv6':
+        proto    => 'udp',
+        port     => ['500','4500'],
+        provider => 'ip6tables',
+      }
+      profile::firewall::rule { "915 ipsec allow protocol ESP":
+        proto    => 'esp',
+#        iniface => $::ipaddress_trp1,
+      }
+      profile::firewall::rule { "915 ipsec allow protocol ESP IPv6":
+        proto    => 'esp',
+        provider => 'ip6tables',
+#        iniface => $::ipaddress_trp1,
+      }
+    }
+  }
+
+}
